4e5ed12cdebf757e8feb4bf4c4127b42159f5e90
ds-kavaliou <ds.kavaliou@gmail.com>
2022-12-10 15:49:14 +0300

    fix: add api key for production test

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from "./loader";
 class AppLoader extends Loader {
   constructor() {
     super("https://newsapi.org/v2/", {
-      apiKey: process.env.API_KEY ?? "",
+      apiKey: process.env.API_KEY ?? "18f1be6a46a1480ebb89a87f93cd6a72",
     });
   }
 }
